9973184b7faf6b2e322265bebada78debd0d54ad
jiangcheng <jiangcheng1806@163.com>
2022-06-29 18:14:32 +0800

    创建一个logback项目

---
+++ src/com/jiangcheng1806/demo/logback/Sample.java
@@ -0,0 +1,13 @@
+package com.jiangcheng1806.demo.logback;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+
+public class Sample {
+	private static final Logger LOGGER = LoggerFactory.getLogger(Sample.class);
+public static void main(String[] args) {
+	LOGGER.info(">>>>>start a loback test");
+	
+}
+}
