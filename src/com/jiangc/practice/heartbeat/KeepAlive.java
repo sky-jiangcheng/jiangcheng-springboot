e924c8cac32cb52759761c225b40403aef222cb8
jiangcheng <jiangcheng1806@163.com>
2023-08-22 17:57:30 +0800

    优化项目，增加其他一些常见测试

---
+++ src/com/jiangc/practice/heartbeat/KeepAlive.java
@@ -0,0 +1,16 @@
+package com.jiangc.practice.heartbeat;
+
+import java.io.Serializable;
+import java.text.SimpleDateFormat;
+import java.util.Date;
+
+public class KeepAlive implements Serializable {
+
+
+    private static final long serialVersionUID = -4558958166553742730L;
+
+    @Override
+    public String toString() {
+        return new SimpleDateFormat("yyyy-MM-dd HH:mm:ss").format(new Date())+"\t维持连接�?";
+    }
+}
